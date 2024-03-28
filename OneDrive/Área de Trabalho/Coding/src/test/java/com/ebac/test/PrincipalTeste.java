5b8e626f9392c1f179d884a983cc005bd962aea6
Isaac Santana <137096445+isaacsantanadevr@users.noreply.github.com>
2024-03-28 19:02:58 -0300

    Update PrincipalTeste.java

--- OneDrive/Área de Trabalho/Coding/src/test/java/com/ebac/test/PrincipalTeste.java
+++ OneDrive/Área de Trabalho/Coding/src/test/java/com/ebac/test/PrincipalTeste.java
@@ -7,19 +7,82 @@ import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class PrincipalTeste{
+    public class ContratoServiceTestTarefa {
 
-    @Test
-    public void TestaMulheres(){
-        List<Pessoas> pessoa = new Pessoas().ListaDePessoas();
-
-        List<Pessoas> mulheres = pessoa.stream()
-                                        .filter(mulher -> mulher.getGen().equals("Mulher"))
-                                        .collect(Collectors.toList());
-
-        for(Pessoas mulher : mulheres){
-            assertEquals("Mulher", mulher.getGen());
+        @Test
+        public void salvarTeste() {
+            IContratoDao dao = new ContratoDaoMock();
+            IContratoService service = new ContratoService(dao);
+            String retorno = service.salvar();
+            Assert.assertEquals("Sucesso", retorno);
         }
+
+        /* Tarefa Criando testes(buscar, excluir, atualizar) */
+
+        @Test
+        public void buscarTeste() {
+            IContratoDao dao = new ContratoDaoMock();
+            IContratoService serviceB = new ContratoService(dao);
+            String retornoB = serviceB.buscar();
+            Assert.assertEquals("Buscando Cliente", retornoB);
+        }
+
+        @Test
+        public void excluirTeste() {
+            IContratoDao dao = new ContratoDaoMock();
+            IContratoService serviceE = new ContratoService(dao);
+            String retornoE = serviceE.excluir();
+            Assert.assertEquals("Cliente Deletado", retornoE);
+        }
+
+        @Test
+        public void atualizarTeste() {
+            IContratoDao dao = new ContratoDaoMock();
+            IContratoService serviceA = new ContratoService(dao);
+            String retornoA = serviceA.atualizar();
+            Assert.assertEquals("Dados atualizados com sucesso", retornoA);
+        }
+
+        /**
+         * @testes de erro
+         */
+
+        @Test(expected = UnsupportedOperationException.class)
+        public void esperadoErroNoSalvarComBancoDeDadosTest() {
+            IContratoDao dao = new ContratoDao();
+            IContratoService service = new ContratoService(dao);
+            String retorno = service.salvar();
+            Assert.assertEquals("Sucesso", retorno);
+        }
+
+        /* Tarefa Criando testes(buscar, excluir, atualizar) */
+
+        @Test(expected = UnsupportedOperationException.class)
+        public void esperadoErrobuscarComBancoDeDadosTest() {
+            IContratoDao dao = new ContratoDao();
+            IContratoService serviceB = new ContratoService(dao);
+            String retornoB = serviceB.buscar();
+            Assert.assertEquals("Buscando Cliente", retornoB);
+        }
+
+        @Test(expected = UnsupportedOperationException.class)
+        public void esperadoErroexcluirtComBancoDeDadosTest() {
+            IContratoDao dao = new ContratoDao();
+            IContratoService serviceE = new ContratoService(dao);
+            String retornoE = serviceE.excluir();
+            Assert.assertEquals("Cliente Deletado", retornoE);
+        }
+
+        @Test(expected = UnsupportedOperationException.class)
+        public void esperadoErroAtualizartComBancoDeDadosTest() {
+            IContratoDao dao = new ContratoDao();
+            IContratoService serviceA = new ContratoService(dao);
+            String retornoA = serviceA.atualizar();
+            Assert.assertEquals("Cliente Deletado", retornoA);
+        }
+
     }
+}
+
 
 }
